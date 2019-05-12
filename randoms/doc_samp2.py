7d005ced2524bb35b80922c91316401304349025
vapawar <vinodpawar8086@gmail.com>
2019-05-13 00:26:34 +0530

    experiments with python

---
+++ randoms/doc_samp2.py
@@ -0,0 +1,8 @@
+# Python 3: List comprehensions
+fruits = ['Banana', 'Apple', 'Lime']
+loud_fruits = [fruit.upper() for fruit in fruits]
+print(loud_fruits)
+
+# List and the enumerate function
+list(enumerate(fruits))
+print(fruits)
