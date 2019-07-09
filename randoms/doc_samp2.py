9c16f5507e3fe0dfc221206d8c083786cfc612d2
vapawar <vinodpawar8086@gmail.com>
2019-07-10 00:36:11 +0530

    remove redundnacy

--- randoms/doc_samp2.py
+++
@@ -1,8 +0,0 @@
-# Python 3: List comprehensions
-fruits = ['Banana', 'Apple', 'Lime']
-loud_fruits = [fruit.upper() for fruit in fruits]
-print(loud_fruits)
-
-# List and the enumerate function
-list(enumerate(fruits))
-print(fruits)
